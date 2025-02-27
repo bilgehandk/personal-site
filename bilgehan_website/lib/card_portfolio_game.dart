e917010cc09317cbff730d0618cfdfadb1ec67d8
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2025-02-27 22:08:31 +0300

    adding animation

--- bilgehan_website/lib/card_portfolio_game.dart
+++ bilgehan_website/lib/card_portfolio_game.dart
@@ -1,3 +1,4 @@
+import 'dart:math'; // for random numbers
 import 'package:flame/effects.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
@@ -5,9 +6,54 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'main.dart'; // Assumes a global "navigatorKey" is defined here
 
-class CardPortfolioGame extends FlameGame {
+// New: Spawner for flying background cards.
+class FlyingCardBackground extends Component with HasGameRef<CardPortfolioGame> {
+  final Random _rand = Random();
+  double spawnTimer = 0;
+  double nextSpawn = 1; // spawn an image every ~1-3 seconds
+
+  @override
+  void update(double dt) {
+    spawnTimer += dt;
+    if (spawnTimer >= nextSpawn) {
+      spawnTimer = 0;
+      nextSpawn = 1 + _rand.nextDouble() * 2;      
+      final x = _rand.nextDouble() * gameRef.size.x;
+      final speed = 100 + _rand.nextDouble() * 100;
+      gameRef.add(FlyingCardComponent(
+        position: Vector2(x, gameRef.size.y + 50),
+        speed: speed,
+      ));
+    }
+  }
+}
+
+// New: Each flying card component.
+class FlyingCardComponent extends SpriteComponent with HasGameRef<CardPortfolioGame> {
+  final double speed;  
+  FlyingCardComponent({required Vector2 position, required this.speed})
+      : super(position: position, size: Vector2(100, 150), priority: -2);
+
   @override
   Future<void> onLoad() async {
+    sprite = await Sprite.load('Cardback.png');
+  }
+
+  @override
+  void update(double dt) {
+    position.add(Vector2(0, -speed * dt));
+    if (position.y + size.y < 0) {
+      removeFromParent();
+    }
+  }
+}
+
+class CardPortfolioGame extends FlameGame with TapDetector {
+  @override
+  Future<void> onLoad() async {
+    // Add animated background first.
+    add(FlyingCardBackground());
+
     final screenWidth = size.x;
     final cardWidth = 150;
     final spacing = (screenWidth - (3 * cardWidth)) / 4;
@@ -47,6 +93,7 @@ class CardComponent extends SpriteComponent with TapCallbacks, HoverCallbacks {
   void onHoverEnter() {
     add(MoveEffect.by(Vector2(0, -10), EffectController(duration: 0.2)));
   }
+  
   @override
   void onHoverExit() {
     add(MoveEffect.by(Vector2(0, 10), EffectController(duration: 0.2)));
